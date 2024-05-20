6c5fe536fd8568d445a0073dd2ad4e1880b87a0b
tommybru <tbru@stanford.edu>
2024-05-20 08:46:45 -0700

    Add vercel server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   // ...
   integrations: [react(), tailwind()],
+  output: "hybrid",
   redirects: {
     '/releases/[...path]': 'https://crowdie.stanford.edu/[...path]'
-  }
+  },
+  adapter: vercel()
 });
