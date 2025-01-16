1c53cec6b8465ca4c31e5b4019797b1ca649117d
tommybru <tbru@stanford.edu>
2025-01-16 19:19:44 +0100

    update 2025 footer

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_Cyh1vqs2.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_D-p1jZeC.mjs');
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/prerender_DuhKFwEX.mjs
@@ -0,0 +1,7 @@
+const noop = () => {};
+export const a = noop;
+export const b = noop;
+export const i = noop;
+export const j = noop;
+export const p = noop;
+export const t = noop;
