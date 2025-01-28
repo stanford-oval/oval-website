820e9bc5663811dbc94a08722eec46a0280156fa
tommybru <tbru@stanford.edu>
2025-01-28 12:01:10 +0100

    remove .vercel/

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/prerender_DuhKFwEX.mjs
+++
@@ -1,7 +0,0 @@
-const noop = () => {};
-export const a = noop;
-export const b = noop;
-export const i = noop;
-export const j = noop;
-export const p = noop;
-export const t = noop;
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/team_BsuBXEET.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_DuhKFwEX.mjs').then(n => n.t);
-
-export { page };
